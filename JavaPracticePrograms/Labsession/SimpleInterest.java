78496b05619fbffcfc7314d3dbda2bf389e34903
SakthiKumar <sakthi.techne@gmail.com>
2025-06-09 14:51:00 +0530

    ne\\sak

--- JavaPracticePrograms/Labsession/SimpleInterest.java
+++ JavaPracticePrograms/Labsession/SimpleInterest.java
@@ -19,6 +19,8 @@ public class SimpleInterest {
         float n=sk.nextFloat();
         float r=sk.nextFloat();
         System.out.println(findSI(p,n,r));
+        System.out.println();
         System.out.println(findCI(p,n,r));
+
     }
 }
