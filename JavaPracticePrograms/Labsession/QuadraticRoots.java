83529ae6e2d06721244a21bd330849e1c6051179
SakthiKumar <sakthi.techne@gmail.com>
2025-06-13 15:02:24 +0530

    last

---
+++ JavaPracticePrograms/Labsession/QuadraticRoots.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class QuadraticRoots {
+    public static void main(String[] args) {
+        Scanner sk=new Scanner(System.in);
+        int a=sk.nextInt();
+        int b=sk.nextInt();
+        int c=sk.nextInt();
+        
+        double discriminant=b*b-(4*a*c);
+        Roots(a,b,c);
+
+  }
+   static void  Roots(int x.int y,int z){
+    
+   }
+
+
+
+    
+}
